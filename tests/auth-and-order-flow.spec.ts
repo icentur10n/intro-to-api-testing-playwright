4a15b3f82c1e6dbcbda26fd28d820888a90c1b94
igorb <igor.barzasekov@gmail.com>
2025-08-11 20:43:56 +0200

    Added negative tests for HW11

--- tests/auth-and-order-flow.spec.ts
+++ tests/auth-and-order-flow.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test'
+import { expect, request, test } from '@playwright/test'
 import { StatusCodes } from 'http-status-codes'
 import { LoginDto } from './dto/login-dto'
 import { OrderDto } from './dto/order-dto'
@@ -7,7 +7,6 @@ const serviceURL = 'https://backend.tallinn-learning.ee/'
 const loginPath = 'login/student'
 const orderPath = 'orders'
 
-// JWT pattern in the form of a regular expression
 const jwtPattern = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
 
 test.describe('Tallinn delivery API tests', () => {
@@ -39,24 +38,62 @@ test.describe('Tallinn delivery API tests', () => {
     expect(responseBody).toBe('')
   })
 
-  // test('login and create order', async ({ request }) => {
-   // const requestBody = LoginDto.createLoginWithCorrectData()
-    //const response = await request.post(`${serviceURL}${loginPath}`, {
-      //data: requestBody,
-    //})
-    //const jwt = await response.text()
-    //const orderResponse = await request.post(`${serviceURL}${orderPath}`, {
-      //data: OrderDto.createOrderWithoutId(),
-      //headers: {
-        //Authorization: `Bearer ${jwt}`,
-      //},
-    //})
+  test('login with incorrect data using wrong PUT http method returns code 405', async ({ request }) => {
+    const requestBody = LoginDto.createLoginWithCorrectData();
+    console.log('requestBody:', requestBody);
+    const response = await request.put(`${serviceURL}${loginPath}`, {
+      data: requestBody,
+    });
+    const responseBody = await response.json();
+    console.log('response code:', response.status());
+    console.log('response body:', responseBody);
+    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED);
+    expect(responseBody.path).toBe(`/${loginPath}`);
+  })
 
-    //const orderResponseBody = await orderResponse.json()
-    //console.log('orderResponse status:', orderResponse.status())
-    //console.log('orderResponse:', orderResponseBody)
-    //expect.soft(orderResponse.status()).toBe(StatusCodes.OK)
-    //expect.soft(orderResponseBody.status).toBe('OPEN')
-    //expect.soft(orderResponseBody.id).toBeDefined()
-  //})
+  test('login with incorrect data using wrong GET HTTP method returns code 405', async ({ request }) => {
+    const response = await request.get(`${serviceURL}${loginPath}`);
+    const responseBody = await response.json();
+
+    console.log('response code:', response.status());
+    console.log('response body:', responseBody);
+
+    expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED);
+    expect(responseBody.path).toBe(`/${loginPath}`);
+  })
+
+  test('unsuccessful login with incorrect data using empty body returns code 401', async ({ request }) => {
+    const invalidRequestBody = {};
+    console.log('invalid request body:', invalidRequestBody);
+    const response = await request.post(`${serviceURL}${loginPath}`, {
+      data: invalidRequestBody,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const responseBody = await response.text();
+
+    console.log('response code:', response.status());
+    console.log('response body:', responseBody);
+    expect(response.status()).toBe(StatusCodes.UNAUTHORIZED);
+    expect(responseBody).toBe('');
+  })
+  test('unsuccessful login with incorrect data using null username and password returns code 401', async ({ request }) => {
+    const invalidRequestBody = { username: null, password: null };
+    console.log('invalid request body:', invalidRequestBody);
+    const response = await request.post(`${serviceURL}${loginPath}`, {
+      data: invalidRequestBody,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const responseBody = await response.text();
+
+    console.log('response code:', response.status());
+    console.log('response body:', responseBody);
+    expect(response.status()).toBe(StatusCodes.UNAUTHORIZED);
+    expect(responseBody).toBe('');
+  });
 })
